3d8a5a31256ae276327a8db7507519dec5eaafd5
hana74 <hanajima01221@gmail.com>
2022-04-01 17:52:46 -0400

    js

--- js/script.js
+++ js/script.js
@@ -37,14 +37,6 @@ button.addEventListener("click", function(){
 
 
 
-// const responseNumber= Math.floor(Math.random()*20)
-// const imageSrc = `./img/magic8ball_${responseNumber}.png`;
-// const responseImage= document.createElement('img');
-// responseImage.src=imageSrc;
-// responseImage.width="100";
-// responseImage.height="100";
-// result.appendChild(responseImage);
-// ​
 
 
 
@@ -62,39 +54,4 @@ button.addEventListener("click", function(){
 
 
 
-// var options = [
-//   "It is certain",
-//   "It is decidedly so",
-//   "Without a doubt",
-//   "Yes – definitely",
-//   "You may rely on it",
-//   "As I see it, yes",
-//   "Most likely",
-//   "Outlook good",
-//   "Yes",
-//   "Signs point to yes",
-//   "Don’t count on it",
-//   "My reply is no",
-//   "My sources say no",
-//   "Outlook not so good",
-//   "Very doubtful",
-//   "Reply hazy, try again",
-//   "Ask again later",
-//   "Better not tell you now",
-//   "Cannot predict now",
-//   "Concentrate and ask again"
-// ];
 
-// button.addEventListener("click", function(){
-//   if (input.value.length < 1) {
-//     alert("Please enter a question!");
-//   } else {
-//     eight.innerText = "";
-//     // var num = input.value.length % options.length;
-//     // answer.innerText = options[num];
-//     const responseNumber= Math.floor(Math.random()*20)
-//     const imageSrc = `options${responseNumber}`;
-//     const responseImage= document.createElement('img');
-    
-//   }
-// });
